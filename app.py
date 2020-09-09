bdaf84a2a91d85833f41c47c51c88eef61e42511
Dop-dd <dopdd@yahoo.com>
2020-09-09 18:41:30 +0000

    sending to heroku test

--- app.py
+++ app.py
@@ -328,4 +328,4 @@ def search_booking():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-                debug=True)
+            debug=True)
