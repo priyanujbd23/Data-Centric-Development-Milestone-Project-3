f53864dacdabf92051f977ffb6d4666c70ffa336
Dop-dd <dopdd@yahoo.com>
2020-09-14 10:06:19 +0000

    pushing to heroku

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ if path.exists("env.py"):
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_DBNAME"] = os.getenv("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
 mongo = PyMongo(app)
 
